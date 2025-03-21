df70f0a46d3f5af52c54f0a223a991c90e99bbf4
SkyKnigth <129749991+SkyKnigth@users.noreply.github.com>
2025-03-21 09:14:13 +0100

    mobile version

--- kasa_PROJECT/src/pages/NotFound.jsx
+++ kasa_PROJECT/src/pages/NotFound.jsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/NotFound.scss";
 
-const NotFound = () => (
-  <main className="not-found">
-    <h1>404</h1>
-    <p>Oups! La page que vous demandez n'existe pas.</p>
-    <Link to="/" className="not-found-link">
-      Retourner sur la page d'accueil
-    </Link>
-  </main>
-);
+const NotFound = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+
+  // Met à jour l'état si la taille de l'écran change
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 600);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <main className="not-found">
+      <h1>404</h1>
+      <p>
+        {isMobile ? (
+          <>
+            Oups! La page que <br /> vous demandez n'existe pas.
+          </>
+        ) : (
+          "Oups! La page que vous demandez n'existe pas."
+        )}
+      </p>
+      <Link to="/" className="not-found-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </main>
+  );
+};
 
 export default NotFound;
